042b1fcc98da8c33b52599176c4a1eeddd3551cb
Tr1st0n <tristonxmoulton@hotmail.com>
2023-06-06 14:35:09 -0400

    Styling Almost Complete

--- src/Form.js
+++ src/Form.js
@@ -1,10 +1,12 @@
 import { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
 
 
 const Form = () => {
 
     const [country, setCountry] = useState('ca');
     const [category, setCategory] = useState('business');
+    const [input, setInput] = useState('')
     const [error, setError] = useState(false);
     const [news, setNews] = useState([]);
 
@@ -15,7 +17,8 @@ useEffect (() => {
     url.search = new URLSearchParams({
     apiKey: apiKey,
     country: country,
-    category: category
+    category: category, 
+    q: input
     });
 
     fetch(url)
@@ -41,10 +44,17 @@ useEffect (() => {
         setNews(news)
     }
 
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log("searching")
+        setInput(e.target.value);
+    }
+
     return (
         <>
-            <label>Show me News from:</label>
+            <label>News from:</label>
             <div
+            className="countries"
             onClick={handleChange}>
                 <button onClick={() => setCountry('ca')}>Canada</button>
                 <button onClick={() => setCountry('us')}>United States</button>
@@ -58,6 +68,7 @@ useEffect (() => {
             </div>
             <label>Categories:</label>
             <div 
+            className="categories"
             onClick={handleChange}>
                 <button onClick={() => setCategory('business')}>Business</button>
                 <button onClick={() => setCategory('entertainment')}>Entertainment</button>
@@ -68,16 +79,24 @@ useEffect (() => {
                 <button onClick={() => setCategory('science')}>Science</button>
                 <button onClick={() => setCategory('sports')}>Sports</button>
                 <button onClick={() => setCategory('technology')}>Technology</button>
-            </div>   
+            </div>  
+            <div>
+                <form>
+                    <label>Search: </label>
+                    <input type="text" id="input"/>
+                    <button onClick={handleSubmit}>Submit</button>
+                </form>
+            </div> 
             <ul>
                 {news.map((article, key)=>(
-                     <li key={key}>
-                        <h3>{article.title}</h3>
+                     <li key={key} className="news-items">
+                        <Link to={article.link}>
+                            <h3>{article.title}</h3>
+                        </Link>
+                        
                         <img src={article.image_url}/>
                         <p>{article.description}</p>                        
                         <p>{`Published on: ${article.pubDate}`}</p>
-                     
-
                     </li>
                 ))}
             </ul>         
--- src/Header.js
+++ src/Header.js
@@ -1,8 +1,10 @@
+import { Link } from "react-router-dom";
+
 const Header = () => {
     return (
         <>
         <h1>Welcome to Code 5 News</h1>
-        <h2>We offer you the latest news from around the World!</h2>
+        <h2>The latest news from around the World!</h2>
         </>
     )
 }
