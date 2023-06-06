5d3a3292b5dfeabf40823f2ba23ed1a71537f925
Tr1st0n <tristonxmoulton@hotmail.com>
2023-06-06 17:45:45 -0400

    Updated Header as well as Media Queries

--- src/Header.js
+++ src/Header.js
@@ -4,7 +4,7 @@ const Header = () => {
     return (
         <>
         <h1>Welcome To Code 5 News</h1>
-        <h2>The latest news from around the World!</h2>
+        <h2>The latest breaking news from around the World!</h2>
         </>
     )
 }
